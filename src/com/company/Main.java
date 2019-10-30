4d00e57a8419b0449ea452551f7a4ebf099620fd
javiOlaya <jjosaez@gmail.com>
2019-10-30 07:54:26 +0100

    Acabado el mostrar la minúscula correspondiente

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-        System.out.println("Dame una mayúscula:");
+        System.out.print("Dame una mayúscula:");
         String letra = scanner.next();
         int n=(int)letra.charAt(0);
-
+        char soluc=(char)(n+32);
+        System.out.print("La minúscula es: "+soluc);
     }
 }
